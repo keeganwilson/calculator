26cc633c075858e876cfb65ae0599885fc0f827c
Keegan Wilson <keegan.wilson@gmail.com>
2022-03-22 16:02:55 -0600

    added additional functionality

--- calc.js
+++ calc.js
@@ -13,17 +13,26 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3])
 
 	if (mathSymbol === '+') {
-		console.log(num1 + num2);
+		console.log(num1 + num2 + num3);
 	}	else if (mathSymbol === '-') {
-		console.log(num1 - num2);
+			console.log(num1 - num2 - num3);
 	}	else if (mathSymbol === '*') {
-		console.log(num1 * num2);
+			console.log(num1 * num2 * num3);
 	}	else if (mathSymbol === '/') {
-		console.log(num1 / num2);
+			console.log(num1 / num2);
 	}	else if (mathSymbol === 'sqrt') {
-		console.log(Math.sqrt(num1));
+			console.log(Math.sqrt(num1));
+	}	else if (mathSymbol === 'sq') {
+			console.log(num1 * num1);
+	}	else if (mathSymbol === 'cube') {
+			console.log(num1 ^ 3);
+	}	else if (mathSymbol === 'power') {
+			console.log(num1 ^ num2);
+	}	else if (mathSymbol === 'remainder') {
+			console.log(num1 % num2)
 	}	else console.log('Please enter a valid operator and number value(s).');
 	
 
